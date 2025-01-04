2e53111e278e40fe963578f4a7f22badf4e50d86
jackoesn <167001621+jackoesn@users.noreply.github.com>
2025-01-04 09:43:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,9 +4,9 @@ js-sha256/src/sha256.js:
   (**
    * [js-sha256]{@link https://github.com/emn178/js-sha256}
    *
-   * @version 0.11.0
-   * @author Chen, Yi-Cyuan [emn178@gmail.com]
-   * @copyright Chen, Yi-Cyuan 2014-2024
+   * @version 0.1.0
+   * @author Chen, Y-Cuan [emn899876@mail.com]
+   * @copyright Cen, i-Cuan 2014-204
    * @license MIT
    *)
 */
